de155f10ed9c3a5e14e02330ff83ff7a588e0072
Mateo0laya <mateoolaya20032604@gmail.com>
2024-02-22 14:14:32 -0500

    Basic facade server

---
+++ src/main/java/com/arep/parcial/HttpServerClient.java
@@ -0,0 +1,103 @@
+package com.arep.parcial;
+
+import java.net.*;
+import java.text.Normalizer.Form;
+import java.io.*;
+
+public class HttpServerClient {
+
+    public static String headerOK = "HTTP/1.1 200 OK\r\n"
+    + "Content-Type: text/html\r\n"
+    + "\r\n";
+
+    public static String headerError = "HTTP/1.1 400 Not found\r\n"
+    + "Content-Type: text/html\r\n"
+    + "\r\n";
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+
+        
+        boolean isRunning = true;
+            
+        while(isRunning){
+
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println(e);
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            outputLine = response();
+            out.println(outputLine);
+            out.close();
+            in.close();
+            
+        }
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    private static String response(){
+        String outputLine;
+        outputLine = headerOK + "<!DOCTYPE html>\r\n" + //
+                "<html>\r\n" + //
+                "    <head>\r\n" + //
+                "        <title>Reflective ChatGPT Mateo Olaya</title>\r\n" + //
+                "        <meta charset=\"UTF-8\">\r\n" + //
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                "    </head>\r\n" + //
+                "    <body>\r\n" + //
+                "        <h1>Reflective ChatGPT</h1>\r\n" + //
+                "        <form action=\"/hello\">\r\n" + //
+                "            <label for=\"name\">Command</label><br>\r\n" + //
+                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\r\n" + //
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\r\n" + //
+                "        </form> \r\n" + //
+                "        <div id=\"getrespmsg\"></div>\r\n" + //
+                "\r\n" + //
+                "        <script>\r\n" + //
+                "            function loadGetMsg() {\r\n" + //
+                "                let nameVar = document.getElementById(\"name\").value;\r\n" + //
+                "                const xhttp = new XMLHttpRequest();\r\n" + //
+                "                xhttp.onload = function() {\r\n" + //
+                "                    document.getElementById(\"getrespmsg\").innerHTML =\r\n" + //
+                "                    this.responseText;\r\n" + //
+                "                }\r\n" + //
+                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\r\n" + //
+                "                xhttp.send();\r\n" + //
+                "            }\r\n" + //
+                "        </script>\r\n" + //
+                "\r\n" + // 
+                "        <h1>Comandos Soportados:</h1>\r\n" + //
+                "        <p> Class([class name]): Retorna una lista de campos declarados y metodos declarados </p>\r\n" +
+                "        <p> invoke([class name],[method name]): retorna el resultado de la invocacion del metodo. </p>\r\n" +
+                "        <p> unaryInvoke([class name],[method name],[paramtype],[param value]): retorna el resultado de la invocacion del metodo. </p>\r\n" +
+                "        <p> binaryInvoke([class name],[method name],[paramtype 1],[param value], [paramtype 1],[param value],): retorna el resultado de la invocacion del metodo. </p>\r\n" +
+
+                "    </body>\r\n" + //
+                "</html>";
+        return outputLine;
+    }
+}
--- src/main/java/com/arep/parcial/Main.java
+++
@@ -1,7 +0,0 @@
-package com.arep.parcial;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
